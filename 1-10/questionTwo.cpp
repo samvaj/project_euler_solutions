79a507ab47a10d6ab12f1d170046478938cff053
Daniel McCormick <danielmccormick99@gmail.com>
2018-09-21 13:53:18 -0400

    Added makefile and Q2

---
+++ 1-10/questionTwo.cpp
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <iostream>
+
+// Question Two: Sum of all even fibonacci numbers bellw 4,000,000
+
+int questionTwo() {
+	int count = 0;
+	int fib[3] = {0,0,1};
+	while (fib[2] < 4000000) {
+		fib[0] = fib[1];
+		fib[1] = fib[2];
+		fib[2] = fib[0]+fib[1];
+		if (fib[2]%2 == 0) {
+			count += fib[2];
+		}
+	}
+	return count;
+}
+
+int main () {
+	printf("The answer to this question is : %i \n", questionTwo());
+	return 0;
+}
